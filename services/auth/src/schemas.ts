ad3e4b8eb0f2100ca0435e8164acbeba52c8c663
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-22 16:58:23 +0600

    prisma first migrateion for `auth` service

--- services/auth/src/schemas.ts
+++ services/auth/src/schemas.ts
@@ -1,12 +1,7 @@
 import { z } from "zod";
 
 export const UserCreateSchema = z.object({
-    authUserId:z.string(),
-    name:z.string(), //name is required
-    email:z.string().email(), //email is required
-    address:z.string().optional(), //address is optional
-    phone:z.string().optional() //phone is optional
+    email:z.string().email(),
+    password:z.string().min(6).max(100),
+    name:z.string().min(3).max(100)
 })
-
-export const UserUpdateSchema = UserCreateSchema.omit({authUserId:true}).partial() //we don't want to update authUserId; baki sob field UserCreateSchema theke copy kore UserUpdateSchema banaychi; and partial() means all field are optional
-
